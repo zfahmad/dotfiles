8c2b764f2fd2ea6e3b38b810e1f642c63ebe93d0
Zaheen Ahmad <zaheen.fa@gmail.com>
2025-02-25 20:22:46 -0700

    Fixed lazydev issue.

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,6 +1,17 @@
 return {
     {
         "neovim/nvim-lspconfig",
+        dependencies = {
+            "folke/lazydev.nvim",
+            ft = "lua", -- only load on lua files
+            opts = {
+                library = {
+                    -- See the configuration section for more details
+                    -- Load luvit types when the `vim.uv` word is found
+                    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                },
+            },
+        },
         config = function()
             -- Changing gutter signs for diagnostics
             local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
@@ -97,7 +108,7 @@ return {
 
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
             local servers = {
-                -- "lua_ls",
+                "lua_ls",
                 "pyright",
                 "jsonls",
                 "bashls",
@@ -118,45 +129,6 @@ return {
                     "clangd",
                     "--fallback-style=webkit",
                     "--offset-encoding=utf-16",
-                },
-                capabilities = capabilities,
-                on_attach = on_attach,
-            })
-            require("lspconfig").lua_ls.setup({
-
-                on_init = function(client)
-                    if client.workspace_folders then
-                        local path = client.workspace_folders[1].name
-                        if
-                            path ~= vim.fn.stdpath("config")
-                            and (vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc"))
-                        then
-                            return
-                        end
-                    end
-
-                    client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-                        runtime = {
-                            -- Tell the language server which version of Lua you're using
-                            -- (most likely LuaJIT in the case of Neovim)
-                            version = "LuaJIT",
-                        },
-                        -- Make the server aware of Neovim runtime files
-                        workspace = {
-                            checkThirdParty = false,
-                            library = {
-                                vim.env.VIMRUNTIME,
-                                -- Depending on the usage, you might want to add additional paths here.
-                                -- "${3rd}/luv/library"
-                                -- "${3rd}/busted/library",
-                            },
-                            -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
-                            -- library = vim.api.nvim_get_runtime_file("", true)
-                        },
-                    })
-                end,
-                settings = {
-                    Lua = {},
                 },
                 capabilities = capabilities,
                 on_attach = on_attach,
